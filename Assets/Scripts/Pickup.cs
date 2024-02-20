9d731acb9e6435d4a981a89392f4e21b84ab6b40
rangasnafu <135566054+rangasnafu@users.noreply.github.com>
2024-02-20 13:52:26 +1100

    Revert "Develop"

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -14,11 +14,6 @@ public class Pickup : MonoBehaviour
     Vector3 objectPos;
     public float distance;
 
-    public GameObject promptUI;
-    public GameObject dropUI;
-
-    public GameObject dialogueUI;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +24,7 @@ public class Pickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canPickup == true && !dialogueUI.activeSelf)
+        if (canPickup == true)
         {
             if (Input.GetKeyDown("e"))
             {
@@ -37,9 +32,6 @@ public class Pickup : MonoBehaviour
                 ObjectIWantToPickup.transform.position = myHands.transform.position;
                 ObjectIWantToPickup.transform.parent = myHands.transform;
                 hasItem = true;
-
-                //promptUI.SetActive(false);
-                dropUI.SetActive(true);
             }
 
         }
@@ -49,33 +41,20 @@ public class Pickup : MonoBehaviour
             ObjectIWantToPickup.transform.parent = null;
             ObjectIWantToPickup.GetComponent<Rigidbody>().AddForce(myHands.transform.forward * throwForce);
             hasItem = false;
-
-            dropUI.SetActive(false);
-        }
-
-        if (Input.GetKeyDown("t") && hasItem == true)
-        {
-            ObjectIWantToPickup.GetComponent<Rigidbody>().isKinematic = false;
-            ObjectIWantToPickup.transform.parent = null;
-            ObjectIWantToPickup.GetComponent<Rigidbody>().AddForce(myHands.transform.forward * throwForce);
-            hasItem = false;
-
-            dropUI.SetActive(false);
         }
 
     }
 
-    private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Acorn" && !dialogueUI.activeSelf)
+        if (other.gameObject.tag == "Acorn")
         {
             if (hasItem == false)
             {
                 canPickup = true;
-                ObjectIWantToPickup = collision.gameObject;
+                ObjectIWantToPickup = other.gameObject;
             }
 
-            promptUI.SetActive(true);
         }
 
     }
@@ -83,7 +62,5 @@ public class Pickup : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         canPickup = false;
-        promptUI.SetActive(false);
-        //dropUI.SetActive(false);
     }
 }
--- Assets/Scripts/ShakeBranch.cs
+++ Assets/Scripts/ShakeBranch.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 public class ShakeBranch : MonoBehaviour
 {
-    public GameObject acornToDrop1;
-    public GameObject acornToDestroy1;
-    public GameObject acornToDrop2;
-    public GameObject acornToDestroy2;
-    public GameObject acornToDrop3;
-    public GameObject acornToDestroy3;
-    public GameObject acornToDrop4;
-    public GameObject acornToDestroy4;
-    public GameObject acornToDrop5;
-    public GameObject acornToDestroy5;
-    public GameObject acornToDrop6;
-    public GameObject acornToDestroy6;
+    public GameObject acornToDrop;
+    public GameObject acornToDestroy;
 
     public GameObject interactPrompt;
 
@@ -25,23 +15,10 @@ public class ShakeBranch : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && canInteract)
         {
-            acornToDrop1.SetActive(true);
-            acornToDestroy1.SetActive(false);
-            acornToDrop2.SetActive(true);
-            acornToDestroy2.SetActive(false);
-            acornToDrop3.SetActive(true);
-            acornToDestroy3.SetActive(false);
-            acornToDrop4.SetActive(true);
-            acornToDestroy4.SetActive(false);
-            acornToDrop5.SetActive(true);
-            acornToDestroy5.SetActive(false);
-            acornToDrop6.SetActive(true);
-            acornToDestroy6.SetActive(false);
-
-
+            acornToDrop.SetActive(true);
+            acornToDestroy.SetActive(false);
             canInteract = false;
             interactPrompt.SetActive(false);
-            Destroy(this.gameObject);
         }
     }
 
@@ -52,13 +29,5 @@ public class ShakeBranch : MonoBehaviour
             canInteract = true;
             interactPrompt.SetActive(true);
         }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            interactPrompt.SetActive(false);
-        } 
     }
 }
