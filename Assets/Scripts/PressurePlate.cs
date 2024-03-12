dd0efa1df1651e6bd1047b643806011a4aac92a9
rangasnafu - Lab 1 <135566054+rangasnafu@users.noreply.github.com>
2024-03-12 13:33:07 +1100

    pressure plate rotates object at changeable angle

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -5,25 +5,57 @@ using UnityEngine;
 public class PressurePlate : MonoBehaviour
 {
     public GameObject gate;
+    public Quaternion rotationAmount;
+    public float speedVariable;
+
+    public bool gateIsOpening = false;
+
+    private float _rotationSpeed = 5f;
+
+    private Quaternion _targetRot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _targetRot = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (gateIsOpening == true && gate.transform.rotation.y <= 87)
+        //{
+        //    gate.transform.Rotate(Vector3.up * rotationAmount.y * Time.deltaTime);
+        //}
+        //else
+        //{
+        //    gate.transform.Rotate(Vector3.up * -rotationAmount.y * Time.deltaTime);
+        //}
+
+        if (gateIsOpening == true)
+        {
+            _targetRot = rotationAmount;
+            //If you want to rotate each time the mouse is clicked
+            //_targetRot *= Quaternion.AngleAxis(-90, transform.forward);
+        }
+        else
+        {
+            _targetRot = Quaternion.identity;
+            //If you want to rotate each time the mouse is clicked
+            //_targetRot *= Quaternion.AngleAxis(90, transform.forward);
+        }
+
+        gate.transform.rotation = Quaternion.Lerp(gate.transform.rotation, _targetRot, _rotationSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Acorn"))
         {
-            Debug.Log("wooooo");
-            gate.transform.Rotate(transform.eulerAngles + new Vector3(0, -90, 0));
+            //Debug.Log("wooooo");
+            //gate.transform.Rotate(transform.eulerAngles + -rotationAmount);
+
+            gateIsOpening = true;
         }
     }
 
@@ -31,7 +63,8 @@ public class PressurePlate : MonoBehaviour
     {
         if (other.CompareTag("Acorn"))
         {
-            gate.transform.Rotate(transform.eulerAngles + new Vector3(0, 90, 0));
+            //gate.transform.Rotate(transform.eulerAngles + rotationAmount);
+            gateIsOpening = false;
         }
     }
 }
